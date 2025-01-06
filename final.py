ce0864292754f6a140795c63919ca0306c70c46f
Du4ando <Du4ando@gmail.com>
2025-01-06 22:52:20 +0500

    Add files via upload

---
+++ final.py
@@ -0,0 +1,20 @@
+username = input ('Введите имя пользователя:')
+title = input ('Введите заголовок заметки:')
+subtitle1 = input('Введите подзаголовок №1')
+subtitle2 = input('Введите подзаголовок №2')
+subtitle3 = input('Введите подзаголовок №3')
+subtitles = [subtitle1, subtitle2 , subtitle3]
+content = input ('описание заметки:')
+status = input ('статус заметки:')
+created_date = input ('дата создания заметки в формате (день-месяц-год):')
+issue_date = input('дедлайн в формате (день-месяц-год):')
+
+note = [username,
+        content,
+        status,
+        created_date,
+        issue_date,
+        title,
+        subtitles
+        ]
+print(note)
